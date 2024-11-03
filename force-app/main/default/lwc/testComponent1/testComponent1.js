e2fe7b23d0065969318cdb7e57dfbfba284d31de
bharathsfdc45 <bharathsfdc45@gmail.com>
2024-11-03 15:31:54 -0500

    New Component and controller

---
+++ force-app/main/default/lwc/testComponent1/testComponent1.js
@@ -0,0 +1,19 @@
+import { LightningElement,wire } from 'lwc';
+import getAccountList from '@salesforce/apex/AccountController.getAccList';
+export default class TestComponent1 extends LightningElement {
+    accounts;
+    error;
+
+    @wire(getAccountList)
+    wiredAccounts({error, data}){
+        if(data){
+            this.accounts = data;
+            this.error = undefined;
+        }
+        else if(error){
+            this.error = error;
+            this.accounts = undefined;
+        }
+
+    }
+}
