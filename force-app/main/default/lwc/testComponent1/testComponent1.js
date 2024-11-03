ba8d31fb4704307a5da93554a9fdb31ebb0d787e
bharathsfdc45 <bharathsfdc45@gmail.com>
2024-11-03 16:10:31 -0500

    MISSING CHANGES

--- force-app/main/default/lwc/testComponent1/testComponent1.js
+++ force-app/main/default/lwc/testComponent1/testComponent1.js
@@ -1,19 +1,18 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getAccountList from '@salesforce/apex/AccountController.getAccList';
-export default class TestComponent1 extends LightningElement {
 
-    //Test123
+export default class TestComponent1 extends LightningElement {
+    @track accounts;
+    @track error;
+
     @wire(getAccountList)
-    //DUMMY COMMIT TO TRIGGER
-    /*wiredAccounts({error, data}){
-        if(data){
+    wiredAccounts({ error, data }) {
+        if (data) {
             this.accounts = data;
             this.error = undefined;
-        }
-        else if(error){
+        } else if (error) {
             this.error = error;
             this.accounts = undefined;
         }
-
-    }*/
+    }
 }
